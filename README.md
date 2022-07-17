ebf2339b8970d5ff71b6c87b972ad9c17d1de497
ValdemirVieira <brenoviera.s@gmail.com>
2022-07-16 22:15:57 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# url-shortener-dio
+ Repositório do Desafio de Projeto - Construindo um Encurtador de URL com NodeJS, TypeScript e MongoDB.
---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://ValdemirVieira:salsa@cluster0.pof0v.mongodb.net/url-shortener-dio?retryWrites=true&w=majority',
+}
